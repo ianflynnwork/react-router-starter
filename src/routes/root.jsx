b0d7648b7be4c6d090531c69d588d3b9c51d4af7
Ian Flynn <ianflynnwork@gmail.com>
2022-09-23 14:43:56 -0400

    s

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Form, Link, Outlet, redirect, useLoaderData } from "react-router-dom";
+import { Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigation } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
 export async function loader() {
@@ -13,6 +13,8 @@ export async function action() {
 
 export default function Root() {
     const { contacts } = useLoaderData();
+    const navigation = useNavigation();
+
     return (
         <>
             <div id="sidebar">
@@ -45,7 +47,12 @@ export default function Root() {
                         <ul>
                             {contacts.map((contact) => (
                                 <li key={contact.id}>
-                                    <Link to={`contacts/${contact.id}`}>
+                                    <NavLink 
+                                        to={`contacts/${contact.id}`}
+                                        className={({ isActive, isPending }) =>
+                                            isActive ? 'active' : isPending ? 'pending' : ''
+                                        }
+                                    >
                                         {contact.first || contact.last ? (
                                             <>
                                                 {contact.first} {contact.last}
@@ -54,7 +61,7 @@ export default function Root() {
                                             <i>No Name</i>
                                         )}{" "}
                                         {contact.favorite && <span>★</span>}
-                                    </Link>
+                                    </NavLink>
                                 </li>
                             ))}
                         </ul>
@@ -65,7 +72,12 @@ export default function Root() {
                     )}
                 </nav>
             </div>
-            <div id="detail">
+            <div 
+                id="detail"
+                className={
+                    navigation.state === 'loading' ? 'loading' : ''
+                }
+            >
                 <Outlet />
             </div>
         </>
